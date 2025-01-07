9d1423e5a02dfa0810c549dd59b938d7ab100c54
shrey <shreya272002gupta@gmail.com>
2025-01-07 10:03:01 +0530

    add calculator program

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Calculator{
+ public static void main(String[]args){
+  //Scanner sc = new Scanner(System.in);
+  System.out.println("Basic Calculator");
+  System.out.println("Choose the operation");
+  System.out.println("1. Addition");
+  System.out.println("2. Subtraction");
+  System.out.println("3. Multiplication");
+  System.out.println("4. Division");
+  
+}
+}
